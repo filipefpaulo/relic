7fc4950474f830bce4146d94b8d80ac94ab09fe9
Filipe <filipef.paulo@gmail.com>
2026-04-11 18:57:00 -0300

    chore: bump version to 0.2.1

--- packages/cli-node/src/bin.debug.ts
+++ packages/cli-node/src/bin.debug.ts
@@ -26,7 +26,7 @@ const program = new Command();
 program
   .name("relic")
   .description("Spec-driven development with a shared artifact layer")
-  .version("0.2.0");
+  .version("0.2.1");
 
 program
   .command("init")
